521e815b466aaff536a4909a300a5f934c766fc2
AlexisFQ <alexis.qf19@gmail.com>
2026-06-15 10:59:43 -0400

    Create site-config.js

---
+++ js/site-config.js
@@ -0,0 +1,18 @@
+window.siteModules = [
+    {
+        id: 'inicio',
+        label: 'P\u00e1gina Inicial',
+    },
+    {
+        id: 'procesos',
+        label: 'Procesos e Indicadores',
+    },
+    {
+        id: 'evaluacion',
+        label: 'Evaluaci\u00f3n 360',
+    },
+    {
+        id: 'datos',
+        label: 'An\u00e1lisis de Datos',
+    },
+];
