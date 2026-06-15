3e71662c45a63dce5585557003191479e3e514d8
AlexisFQ <alexis.qf19@gmail.com>
2026-06-15 10:59:34 -0400

    Create home-links.js

---
+++ js/home-links.js
@@ -0,0 +1,66 @@
+window.homeLinks = {
+    'equipo-pj': [
+        {
+            label: 'Actas 2026',
+            url: '#',
+        },
+        {
+            label: 'Tareas [Lista]',
+            url: '#',
+        },
+        {
+            label: 'Tareas [Formulario]',
+            url: '#',
+        },
+        {
+            label: 'Prompts [N\u00f3mina]',
+            url: '#',
+        },
+    ],
+    pepsi: [
+        {
+            label: '[DOC]',
+            url: '#',
+        },
+        {
+            label: '[Planillas]',
+            url: '#',
+        },
+        {
+            label: '[Panel Monitoreo]',
+            url: '#',
+        },
+        {
+            label: 'Hoja de Ruta 2024-2026',
+            url: '#',
+        },
+    ],
+    calendarios: [
+        {
+            label: 'Calendario Inspectorial 2026',
+            url: '#',
+        },
+        {
+            label: 'Calendario 2026 [Obras]',
+            url: '#',
+        },
+        {
+            label: 'Viajes / Visitas [Interno]',
+            url: '#',
+        },
+    ],
+    'casa-inspectorial': [
+        {
+            label: 'Espacios C. Inspectorial',
+            url: '#',
+        },
+        {
+            label: 'Salidas / permisos',
+            url: '#',
+        },
+        {
+            label: 'Solicitud de gastos',
+            url: '#',
+        },
+    ],
+};
